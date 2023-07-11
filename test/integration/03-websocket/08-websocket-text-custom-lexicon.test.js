bcc3cea4c965ad0ba13e64d205917a04e3fa3685
Sebastien Robert <sebastien.robert@myscript.com>
2023-07-11 13:57:29 +0200

    feat/lexicon

---
+++ test/integration/03-websocket/08-websocket-text-custom-lexicon.test.js
@@ -0,0 +1,47 @@
+const { write, getExportedDatas, waitEditorLoaded, waitForEditorWebSocket } = require("../helper")
+const { testGesture } = require("../_partials/gesture-test")
+const { h, klopmo } = require("../strokesDatas")
+
+describe("Websocket Text Custom Lexicon", () => {
+  beforeAll(async () => {
+    await page.goto("/examples/websocket/websocket_text_custom_lexicon.html")
+  })
+
+  beforeEach(async () => {
+    await page.reload()
+    await waitEditorLoaded(page)
+  })
+
+  test("should have title", async () => {
+    const title = await page.title()
+    expect(title).toMatch("Custom lexicon")
+  })
+
+  test("should export application/vnd.myscript.jiix", async () => {
+    const [exports] = await Promise.all([
+      getExportedDatas(page),
+      write(page, h.strokes),
+    ])
+    const jiixExpected = h.exports["application/vnd.myscript.jiix"]
+    const jiixReceived = JSON.parse(exports["application/vnd.myscript.jiix"])
+    expect(jiixReceived).toStrictEqual(jiixExpected)
+  })
+
+  test("should send lexicon data with klopmo", async () => {
+    await page.locator("#lexicon").fill("klopmo")
+    await page.click(`#reinit`)
+    await waitForEditorWebSocket(page)
+
+    const [exports] = await Promise.all([
+      getExportedDatas(page),
+      write(page, klopmo.strokes),
+    ])
+    const jiixExpected = klopmo.exports["application/vnd.myscript.jiix"]
+    const jiixReceived = JSON.parse(exports["application/vnd.myscript.jiix"])
+    expect(jiixReceived.label).toStrictEqual(jiixExpected.label)
+  })
+
+  require("../_partials/smart-guide-test")
+
+  testGesture(-100)
+})
