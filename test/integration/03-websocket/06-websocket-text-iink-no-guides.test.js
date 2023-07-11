f154e7ed9b1ab52a4f703381a41575b5688e7a1c
Sebastien Robert <sebastien.robert@myscript.com>
2023-07-11 09:46:00 +0200

    feat/text-noguides

---
+++ test/integration/03-websocket/06-websocket-text-iink-no-guides.test.js
@@ -0,0 +1,42 @@
+const {
+    write,
+    getExportedDatas,
+    waitForEditorWebSocket,
+  } = require("../helper")
+  const { helloOneStroke } = require("../strokesDatas")
+
+  describe("Websocket Text Search Without Smartguide", () => {
+    beforeAll(async () => {
+      await page.goto("/examples/websocket/websocket_text_iink_no_guides.html")
+    })
+
+    beforeEach(async () => {
+      await page.reload({ waitUntil: "networkidle" })
+      await waitForEditorWebSocket(page)
+    })
+
+    test("should have title", async () => {
+      const title = await page.title()
+      expect(title).toMatch("No guides")
+    })
+
+    test("should draw stroke hello", async () => {
+      const [exports] = await Promise.all([
+        getExportedDatas(page),
+        write(page, helloOneStroke.strokes),
+      ])
+
+      const jiixExpected = helloOneStroke.exports["application/vnd.myscript.jiix"].label
+      const jiixReceived = JSON.parse(exports["application/vnd.myscript.jiix"]).label
+      expect(jiixReceived).toStrictEqual(jiixExpected)
+    })
+
+    test("should not see guides", async () => {
+        const [exports] = await Promise.all([
+            getExportedDatas(page),
+            write(page, helloOneStroke.strokes),
+        ])
+        const line = await page.locator("line").all()
+        expect(line.length).toEqual(0)
+    })
+  })
