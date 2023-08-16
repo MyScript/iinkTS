2b3f9c2af084df55e705b99b5af8a6aaf3c78496
Sebastien Robert <sebastien.robert@myscript.com>
2023-08-16 09:18:28 +0200

    feat/file_export

---
+++ test/integration/03-websocket/websocket-text-file-export.test.js
@@ -0,0 +1,71 @@
+const { write, getExportedDatas, waitForEditorWebSocket } = require("../helper")
+const { hello } = require("../strokesDatas")
+
+describe("Websocket Text file export", () => {
+  beforeAll(async () => {
+    await page.goto("/examples/websocket/websocket_text_file_export.html")
+  })
+
+  beforeEach(async () => {
+    await page.reload({ waitUntil: "networkidle" })
+    await waitForEditorWebSocket(page)
+    await page.waitForTimeout(1000)
+  })
+
+  test("should have title", async () => {
+    const title = await page.title()
+    expect(title).toMatch("Word Export with iink")
+  })
+
+  test("should write and save to word file", async () => {
+    await waitForEditorWebSocket(page)
+    await Promise.all([
+      getExportedDatas(page),
+      write(page, hello.strokes)
+    ])
+    const downloadPromise = page.waitForEvent('download');
+    await page.locator("#exportContent").click()
+    const download = await downloadPromise;
+    expect(download.suggestedFilename()).toBe("myDocument.docx");
+  })
+
+  test("should write and save to html file", async () => {
+    await waitForEditorWebSocket(page)
+    await Promise.all([
+      getExportedDatas(page),
+      write(page, hello.strokes)
+    ])
+    const downloadPromise = page.waitForEvent('download');
+    await page.selectOption('#exportType', 'html'),
+    await page.locator("#exportContent").click()
+    const download = await downloadPromise;
+    expect(download.suggestedFilename()).toBe("myDocument.html");
+  })
+
+  test("should write and save to png file", async () => {
+    await waitForEditorWebSocket(page)
+    await Promise.all([
+      getExportedDatas(page),
+      write(page, hello.strokes)
+    ])
+    const downloadPromise = page.waitForEvent('download');
+    await page.selectOption('#exportType', 'png'),
+    await page.locator("#exportContent").click()
+    const download = await downloadPromise;
+    expect(download.suggestedFilename()).toBe("myDocument.png");
+  })
+
+  test("should write and save to jpg file", async () => {
+    await waitForEditorWebSocket(page)
+    await Promise.all([
+      getExportedDatas(page),
+      write(page, hello.strokes)
+    ])
+    const downloadPromise = page.waitForEvent('download');
+    await page.selectOption('#exportType', 'jpeg'),
+    await page.locator("#exportContent").click()
+    const download = await downloadPromise;
+    expect(download.suggestedFilename()).toBe("myDocument.jpg");
+  })
+
+})
