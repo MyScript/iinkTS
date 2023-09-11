9e04d5663d1a42a7301ad56d5260fcb2d5cdcdd5
Jocelyn Boureau <jocelyn.boureau@myscript.com>
2023-09-11 11:07:43 +0200

    feat/math_import_jiix

---
+++ test/integration/03-websocket/websocket-math-import-jiix.test.js
@@ -0,0 +1,25 @@
+
+const { waitForEditorWebSocket, waitEditorIdle, getEditorModelExportsType, haveSameLabels } = require('../helper')
+
+describe('Websocket Math Import JIIX', function () {
+  beforeAll(async () => {
+    await page.goto('/examples/websocket/websocket_math_import_jiix.html')
+    await waitForEditorWebSocket(page)
+  })
+
+  test('should have title', async () => {
+    const title = await page.title()
+    expect(title).toMatch('Import math with JIIX')
+  })
+
+  test('should import JIIX', async () => {
+    await page.click('#import')
+    await waitEditorIdle(page)
+
+    const jiix = await getEditorModelExportsType(page, "application/vnd.myscript.jiix")
+    const jiixTextToImport = await page.locator("#jiix").textContent()
+    const jiixToImport = JSON.parse(jiixTextToImport)
+    expect(haveSameLabels(jiix, jiixToImport)).toEqual(true)
+  })
+
+})
