6745046e7cc8482c1296c664f91cac76fc2b99e0
Olivier BEROT <olivier.berot@myscript.com>
2023-10-20 16:05:58 +0200

    added rest no ui test

---
+++ test/integration/02-rest/rest-no-ui.test.js
@@ -0,0 +1,31 @@
+const {
+  getDatasFromExportedEvent,
+} = require('../helper')
+
+describe('Rest no UI', () => {
+
+  beforeAll(async () => {
+    await page.goto('/examples/rest/rest_no_ui.html')
+  })
+
+  test('should have title', async () => {
+    const title = await page.title()
+    expect(title).toMatch('REST no UI')
+  })
+
+  test('should display text/plain into result', async () => {
+    expect(await page.locator('#interpretatedTextContent').textContent()).toBe('')
+    expect(await page.locator('#interpretatedImageContent > *').count()).toEqual(0)    
+    const textPlainExport =  page.waitForResponse(async (resp) => {
+      const headers = await resp.allHeaders()
+      return resp.url().includes('/iink/batch') && (headers['content-type'] == 'text/plain')
+    })
+    const imagePngExport = page.waitForResponse(async (resp) => {
+      return resp.url().includes('/iink/batch') && (await resp.allHeaders())['content-type'] == 'image/png'
+    })
+    await Promise.all([textPlainExport, imagePngExport, page.click('#recognize')])
+    expect(await page.locator('#interpretatedTextContent').textContent()).not.toBe('')
+    expect(await page.locator('#interpretatedImageContent > *').count()).toEqual(1)    
+    expect(await page.locator('#interpretatedImageContent > img').getAttribute('src')).toContain('data:image/png;base64')
+  })
+})
