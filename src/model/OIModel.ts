5e61766f6fdd971f90863cb89e3b2ae9f96f572e
Jocelyn Boureau <jocelyn.boureau@myscript.com>
2023-12-18 14:55:48 +0100

    feat(offscreen) add OIModel - IIC-975

---
+++ src/model/OIModel.ts
@@ -0,0 +1,210 @@
+import { LoggerClass, WriteTool } from "../Constants"
+import { LoggerManager } from "../logger"
+import { OIStroke, SymbolType, TOISymbol, TPointer } from "../primitive"
+import { TStyle } from "../style"
+import { TExport } from "./Export"
+import { IModel, TRecognitionPositions } from "./IModel"
+
+/**
+ * @group Model
+ */
+export class OIModel implements IModel
+{
+  #logger = LoggerManager.getLogger(LoggerClass.MODEL)
+  readonly creationTime: number
+  modificationDate: number
+  positions: TRecognitionPositions
+  currentSymbol?: TOISymbol
+  symbols: TOISymbol[]
+  exports?: TExport
+  converts?: TExport
+  width: number
+  height: number
+  rowHeight: number
+  idle: boolean
+
+  constructor(width = 100, height = 100, rowHeight = 0, creationDate = Date.now())
+  {
+    this.creationTime = creationDate
+    this.modificationDate = creationDate
+    this.width = width
+    this.height = height
+    this.rowHeight = rowHeight
+    this.symbols = []
+    this.positions = {
+      lastSentPosition: 0,
+      lastReceivedPosition: 0
+    }
+    this.idle = true
+  }
+
+  get selection(): TOISymbol[]
+  {
+    return this.symbols.filter(s => s.selected)
+  }
+
+  selectSymbol(id: string): void
+  {
+    const symbol = this.symbols.find(s => s.id === id)
+    if (symbol) {
+      symbol.selected = true
+    }
+  }
+
+  unselectSymbol(id: string): void
+  {
+    const symbol = this.symbols.find(s => s.id === id)
+    if (symbol) {
+      symbol.selected = false
+    }
+  }
+
+  resetSelection(): void
+  {
+    this.symbols.forEach(s => s.selected = false)
+  }
+
+  createCurrentSymbol(tool: WriteTool, pointer: TPointer, style: TStyle, pointerId: number, pointerType: string): void
+  {
+    this.#logger.info("initCurrentStroke", { tool, pointer, style, pointerId, pointerType })
+    switch (tool) {
+      default:
+        this.currentSymbol = new OIStroke(style, pointerId, pointerType)
+        break
+    }
+    this.updateCurrentSymbol(pointer)
+  }
+
+  updateCurrentSymbol(pointer: TPointer): TOISymbol
+  {
+    this.#logger.info("updateCurrentSymbol", { pointer })
+    if (!this.currentSymbol) {
+      throw new Error("Can't update current symbol because currentSymbol is undefined")
+    }
+
+    switch (this.currentSymbol?.type) {
+      case SymbolType.Stroke:
+        (this.currentSymbol as OIStroke).addPointer(pointer)
+        break
+      default:
+        this.#logger.warn("updateCurrentSymbol", `currentSymbol type is unknow: "${ this.currentSymbol.type }"`)
+        break
+    }
+    this.modificationDate = Date.now()
+    this.exports = undefined
+    return this.currentSymbol
+  }
+
+  endCurrentSymbol(pointer: TPointer): TOISymbol
+  {
+    this.#logger.info("endCurrentStroke", { pointer })
+    const symbol = this.updateCurrentSymbol(pointer)
+    this.currentSymbol = undefined
+    this.addSymbol(symbol)
+    return symbol
+  }
+
+  addSymbol(symbol: TOISymbol): void
+  {
+    this.#logger.info("addSymbol", { symbol })
+    this.symbols.push(symbol)
+    this.modificationDate = Date.now()
+    this.converts = undefined
+    this.exports = undefined
+    this.#logger.debug("addSymbol", this.symbols)
+  }
+
+  updateSymbol(updatedSymbol: TOISymbol): void
+  {
+    this.#logger.info("updateSymbol", { updatedSymbol })
+    const sIndex = this.symbols.findIndex(s => s.id === updatedSymbol.id)
+    if (sIndex !== -1) {
+      this.symbols.splice(sIndex, 1, updatedSymbol)
+      this.modificationDate = Date.now()
+      this.converts = undefined
+      this.exports = undefined
+    }
+    this.#logger.debug("updateSymbol", this.symbols)
+  }
+
+  removeSymbol(id: string): void
+  {
+    this.#logger.info("removeSymbol", { id })
+    const strokeIndex = this.symbols.findIndex(s => s.id === id)
+    if (strokeIndex !== -1) {
+      this.positions.lastSentPosition--
+      this.positions.lastReceivedPosition--
+      this.symbols.splice(strokeIndex, 1)
+      this.modificationDate = Date.now()
+      this.converts = undefined
+      this.exports = undefined
+    }
+    this.#logger.debug("removeSymbol", this.symbols)
+  }
+
+  updatePositionSent(position: number = this.symbols.length): void
+  {
+    this.#logger.info("updatePositionSent", { position })
+    this.positions.lastSentPosition = position
+    this.#logger.debug("updatePositionSent", this.positions.lastSentPosition)
+  }
+
+  updatePositionReceived(): void
+  {
+    this.#logger.info("updatePositionReceived")
+    this.positions.lastReceivedPosition = this.positions.lastSentPosition
+    this.#logger.debug("updatePositionReceived", this.positions.lastReceivedPosition)
+  }
+
+  extractUnsentStrokes(): OIStroke[]
+  {
+    return this.symbols.slice(this.positions.lastSentPosition).filter(s => s.type === SymbolType.Stroke) as OIStroke[]
+  }
+
+  extractDifferenceSymbols(model: OIModel): { added: TOISymbol[], removed: TOISymbol[] }
+  {
+    return {
+      added: this.symbols.filter(s1 => model.symbols.findIndex(s2 => s1.id === s2.id && s1.modificationDate === s2.modificationDate) === -1),
+      removed: model.symbols.filter(s1 => this.symbols.findIndex(s2 => s1.id === s2.id && s1.modificationDate === s2.modificationDate) === -1)
+    }
+  }
+
+  mergeExport(exports: TExport)
+  {
+    this.#logger.info("mergeExport", { exports })
+    if (this.exports) {
+      Object.assign(this.exports, exports)
+    } else {
+      this.exports = exports
+    }
+    this.#logger.debug("mergeExport", this.exports)
+  }
+
+  getClone(): OIModel
+  {
+    this.#logger.info("getClone")
+    const clonedModel = new OIModel(this.width, this.height, this.rowHeight, this.creationTime)
+    clonedModel.modificationDate = this.modificationDate
+    clonedModel.currentSymbol = this.currentSymbol ? this.currentSymbol.getClone() : undefined
+    clonedModel.symbols = this.symbols.map(s => s.getClone())
+    clonedModel.positions = structuredClone(this.positions)
+    clonedModel.exports = structuredClone(this.exports)
+    clonedModel.idle = this.idle
+    this.#logger.debug("getClone", { clonedModel })
+    return clonedModel
+  }
+
+  clear(): void
+  {
+    this.#logger.info("clear")
+    this.modificationDate = Date.now()
+    this.currentSymbol = undefined
+    this.symbols = []
+    this.positions.lastSentPosition = 0
+    this.positions.lastReceivedPosition = 0
+    this.exports = undefined
+    this.converts = undefined
+    this.idle = true
+
+  }
+}
---
+++ test/unit/model/OIModel.test.ts
@@ -0,0 +1,329 @@
+import { buildOIStroke, delay } from "../helpers"
+
+import
+{
+  OIModel,
+  WriteTool,
+  OIStroke,
+  TPointer,
+  TStyle,
+  DefaultStyle,
+  SymbolType,
+  TExport
+} from "../../../src/iink"
+
+
+
+describe("OIModel.ts", () =>
+{
+  const width = 100, height = 100, rowHeight = 10
+  test("should create", () =>
+  {
+    const model = new OIModel(width, height, rowHeight)
+    expect(model).toBeDefined()
+  })
+
+  describe("currentSymbol", () =>
+  {
+    const model = new OIModel(width, height, rowHeight)
+
+    test("should createCurrentSymbol with pencil", async () =>
+    {
+      expect(model.currentSymbol).toBeUndefined()
+      const point: TPointer = { t: 1, p: 0.5, x: 1, y: 1 }
+      expect(model.creationTime).toStrictEqual(model.modificationDate)
+      await delay(100)
+      model.createCurrentSymbol(WriteTool.Pencil, point, DefaultStyle, 42, "mouse")
+      expect(model.modificationDate - model.creationTime).toBeGreaterThanOrEqual(100)
+      expect(model.currentSymbol).toBeDefined()
+      expect(model.currentSymbol?.style.color).toBe(DefaultStyle.color)
+      expect(model.currentSymbol?.style.width).toBe(DefaultStyle.width)
+      expect(model.currentSymbol?.type).toBe(SymbolType.Stroke)
+      const stroke = model.currentSymbol as OIStroke
+      expect(stroke.pointers).toHaveLength(1)
+      expect(stroke.pointers[0].x).toBe(point.x)
+      expect(stroke.pointers[0].y).toBe(point.y)
+      expect(stroke.pointers[0].t).toBe(point.t)
+      expect(stroke.pointers[0].p).toBe(point.p)
+    })
+    test("should createCurrentSymbol with pencil & custom style", async () =>
+    {
+      const _model = new OIModel(width, height, rowHeight)
+      const point: TPointer = { t: 1, p: 0.5, x: 1, y: 1 }
+      const style: TStyle = { color: "red", width: 42 }
+      expect(_model.creationTime).toStrictEqual(_model.modificationDate)
+      await delay(101)
+      _model.createCurrentSymbol(WriteTool.Pencil, point, style, 42, "mouse")
+      expect(_model.modificationDate - _model.creationTime).toBeGreaterThanOrEqual(100)
+      expect(_model.currentSymbol).toBeDefined()
+      expect(_model.currentSymbol?.style.color).toBe(style.color)
+      expect(_model.currentSymbol?.style.width).toBe(style.width)
+    })
+    test("should updateCurrentSymbol", () =>
+    {
+      expect(model.currentSymbol).toBeDefined()
+      const point: TPointer = { t: 15, p: 15, x: 15, y: 15 }
+      model.updateCurrentSymbol(point)
+      const stroke = model.currentSymbol as OIStroke
+      expect(stroke.pointers).toHaveLength(2)
+      expect(stroke.pointers[1].x).toBe(point.x)
+      expect(stroke.pointers[1].y).toBe(point.y)
+      expect(stroke.pointers[1].t).toBe(point.t)
+      expect(stroke.pointers[1].p).toBe(point.p)
+    })
+    test("should endCurrentSymbol", () =>
+    {
+      expect(model.currentSymbol).toBeDefined()
+      const currentSymbol = { ...model.currentSymbol }
+      const point: TPointer = { t: 25, p: 25, x: 25, y: 25 }
+      expect(model.symbols).toHaveLength(0)
+      model.endCurrentSymbol(point)
+      expect(model.currentSymbol).toBeUndefined()
+      expect(model.symbols).toHaveLength(1)
+      expect(model.symbols[0].id).toEqual(currentSymbol.id)
+    })
+    test("should throw error when updateCurrentSymbol if currentSymbol undefined", () =>
+    {
+      expect(model.currentSymbol).toBeUndefined()
+      const point: TPointer = { t: 15, p: 15, x: 15, y: 15 }
+      expect(() => model.updateCurrentSymbol(point)).toThrow("Can't update current symbol because currentSymbol is undefined")
+    })
+  })
+
+  describe("symbols", () =>
+  {
+    const model = new OIModel(27, 5)
+    const symb = buildOIStroke()
+    test("should addSymbol", () =>
+    {
+      expect(model.symbols).toHaveLength(0)
+      model.addSymbol(symb)
+      expect(model.symbols).toHaveLength(1)
+      expect(model.symbols[0]).toEqual(symb)
+    })
+    test("should updateSymbol", () =>
+    {
+      const updatedSymb = structuredClone(symb)
+      updatedSymb.style.color = "yellow"
+      updatedSymb.style.width = 25
+      updatedSymb.pointers.push({ p: 1, t: 20, x: 42, y: 31})
+      model.updateSymbol(updatedSymb)
+      expect(model.symbols[0]).not.toEqual(symb)
+      expect(model.symbols[0]).toEqual(updatedSymb)
+    })
+    test("should not updateSymbol if id not exist", () =>
+    {
+      const symb2 = buildOIStroke()
+      model.addSymbol(symb2)
+      const updatedSymb2 = structuredClone(symb2)
+      updatedSymb2.id = "not-exist"
+      updatedSymb2.style.color = "yellow"
+      updatedSymb2.style.width = 25
+      updatedSymb2.pointers.push({ p: 1, t: 20, x: 42, y: 31})
+      model.updateSymbol(updatedSymb2)
+      expect(model.symbols[1]).toEqual(symb2)
+    })
+    test("should removeSymbol", () =>
+    {
+      expect(model.symbols).toHaveLength(2)
+      model.removeSymbol(symb.id)
+      expect(model.symbols).toHaveLength(1)
+    })
+    test("should not removeSymbol if id not exist", () =>
+    {
+      expect(model.symbols).toHaveLength(1)
+      model.removeSymbol("not-exist")
+      expect(model.symbols).toHaveLength(1)
+    })
+  })
+
+  describe("selection", () =>
+  {
+    const model = new OIModel(27, 5)
+    const stroke1 = buildOIStroke()
+    model.addSymbol(stroke1)
+    const stroke2 = buildOIStroke()
+    model.addSymbol(stroke2)
+    test("should select stroke when point on stroke", () =>
+    {
+      model.selectSymbol(stroke1.id)
+      expect(model.selection).toHaveLength(1)
+      expect(model.selection[0].id).toEqual(stroke1.id)
+    })
+    test("should unselect stroke", () =>
+    {
+      expect(model.selection).toHaveLength(1)
+      model.unselectSymbol(stroke1.id)
+      expect(model.selection).toHaveLength(0)
+    })
+    test("should not select the same stroke twice", () =>
+    {
+      model.selectSymbol(stroke1.id)
+      expect(model.selection).toHaveLength(1)
+      model.selectSymbol(stroke1.id)
+      expect(model.selection).toHaveLength(1)
+    })
+    test("should reset selection", () =>
+    {
+      expect(model.selection).toHaveLength(1)
+      model.resetSelection()
+      expect(model.selection).toHaveLength(0)
+    })
+  })
+
+  describe("positions", () =>
+  {
+    const model = new OIModel(27, 5)
+    model.addSymbol(buildOIStroke())
+    model.addSymbol(buildOIStroke())
+    test("should positions init with 0", () =>
+    {
+      expect(model.positions).toEqual({ lastSentPosition: 0, lastReceivedPosition: 0 })
+    })
+
+    test("should update position sent", () =>
+    {
+      model.updatePositionSent()
+      expect(model.positions).toEqual({ lastSentPosition: 2, lastReceivedPosition: 0 })
+    })
+    test("should update received sent", () =>
+    {
+      model.updatePositionReceived()
+      expect(model.positions).toEqual({ lastSentPosition: 2, lastReceivedPosition: 2 })
+    })
+  })
+
+  describe("extract", () =>
+  {
+    const model = new OIModel(27, 5)
+    model.addSymbol(buildOIStroke())
+    model.addSymbol(buildOIStroke())
+    test("should extract all traits if nothing is sent", () =>
+    {
+      expect(model.extractUnsentStrokes()).toEqual(model.symbols)
+    })
+    test("should extract a stroke if one out of two sent", () =>
+    {
+      model.positions.lastSentPosition = 1
+      expect(model.extractUnsentStrokes()).toEqual([model.symbols[1]])
+    })
+    test("should not extract anything if all strokes have been sent", () =>
+    {
+      model.positions.lastSentPosition = 2
+      expect(model.extractUnsentStrokes()).toHaveLength(0)
+    })
+
+    test("should extract symbol added in second model", () =>
+    {
+      const newModel = model.getClone()
+      const newSymb = buildOIStroke()
+      model.addSymbol(newSymb)
+      const { added, removed } = model.extractDifferenceSymbols(newModel)
+      expect(added).toEqual([newSymb])
+      expect(removed).toEqual([])
+    })
+    test("should extract symbol removed in second model", () =>
+    {
+      const newModel = model.getClone()
+      const newSymb = buildOIStroke()
+      model.addSymbol(newSymb)
+      const { added, removed } = newModel.extractDifferenceSymbols(model)
+      expect(added).toEqual([])
+      expect(removed).toEqual([newSymb])
+    })
+    test("should extract nothing if same model", () =>
+    {
+      const { added, removed } = model.extractDifferenceSymbols(model)
+      expect(added).toEqual([])
+      expect(removed).toEqual([])
+    })
+  })
+
+  describe("export", () =>
+  {
+    const model = new OIModel(27, 5)
+    const e: TExport = { "text/plain": "poney" }
+    test("should define export to undefined", () =>
+    {
+      expect(model.exports).toBeUndefined()
+    })
+    test("should init export", () =>
+    {
+      model.mergeExport(e)
+      expect(model.exports).toEqual(e)
+    })
+    test("should merge export", () =>
+    {
+      const newExport: TExport = {
+        "application/vnd.myscript.jiix": {
+          "type": "Text",
+          "label": "poney",
+          "words": [
+            {
+              "label": "poney",
+              "candidates": ["poney", "Poney", "ponay", "ponex", "pony"]
+            }
+          ],
+          "version": "3",
+          "id": "MainBlock"
+        }
+      }
+
+      model.mergeExport(newExport)
+      expect(model.exports).toMatchObject(newExport)
+      expect(model.exports).toMatchObject(e)
+    })
+  })
+
+  describe("clone", () =>
+  {
+    const model = new OIModel(27, 5)
+    const stroke = buildOIStroke()
+    model.addSymbol(stroke)
+    model.createCurrentSymbol(WriteTool.Pencil, { p: 27, t: 5, x: 1989, y: 42 }, DefaultStyle, 42, "mouse")
+    model.exports = { "text/plain": "M" }
+    test("should getClone", () =>
+    {
+      const clone = model.getClone()
+      expect(clone != model).toBeTruthy()
+      expect(clone.currentSymbol).toEqual(model.currentSymbol)
+      expect(clone.currentSymbol).not.toBe(model.currentSymbol)
+
+      expect(clone.exports).toEqual(model.exports)
+      expect(clone.exports).not.toBe(model.exports)
+
+      expect(clone.height).toEqual(model.height)
+
+      expect(clone.idle).toEqual(model.idle)
+
+      expect(clone.positions).toEqual(model.positions)
+      expect(clone.positions).not.toBe(model.positions)
+
+      expect(clone.symbols).toEqual(model.symbols)
+      expect(clone.symbols).not.toBe(model.symbols)
+
+      expect(clone.width).toEqual(model.width)
+    })
+  })
+
+  describe("clear", () =>
+  {
+    test("should clear model", () =>
+    {
+      const model = new OIModel(width, height, rowHeight)
+      const p1: TPointer = { t: 1, p: 1, x: 1, y: 1 }
+      const p2: TPointer = { t: 10, p: 10, x: 10, y: 10 }
+      const p3: TPointer = { t: 10, p: 10, x: 10, y: 10 }
+
+      model.createCurrentSymbol(WriteTool.Pencil, p1, DefaultStyle, 42, "mouse")
+      model.endCurrentSymbol(p2)
+      model.createCurrentSymbol(WriteTool.Pencil, p3, DefaultStyle, 51, "mouse")
+      expect(model.currentSymbol).toBeDefined()
+      expect(model.symbols).toHaveLength(1)
+
+      model.clear()
+      expect(model.currentSymbol).toBeUndefined()
+      expect(model.symbols).toHaveLength(0)
+    })
+  })
+})
