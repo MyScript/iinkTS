59ab72bcebb2297f531a07d5d952b752d741b847
Jocelyn Boureau <jocelyn.boureau@myscript.com>
2024-07-29 09:11:46 +0200

    fix(OISymbolGroup) update style on each children

--- src/primitive/OISymbolGroup.ts
+++ src/primitive/OISymbolGroup.ts
@@ -56,6 +56,9 @@ export class OISymbolGroup extends OISymbolBase<SymbolType.Group>
           }
         })
       }
+      else if (child.type === SymbolType.Group) {
+        child.updateChildrenStyle()
+      }
     })
   }
 
