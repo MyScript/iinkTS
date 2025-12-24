1734da0e7f45f417f937deea22bc0c23e5844157
Jocelyn Boureau <jocelyn.boureau@myscript.com>
2025-12-24 15:19:51 +0100

    fix: update beforeEach to fill additional keys and save in Offscreen TLDraw tests

--- test/examples/04-offscreen/TLDraw.test.js
+++ test/examples/04-offscreen/TLDraw.test.js
@@ -6,13 +6,19 @@ test.describe("Offscreen TLDraw", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(`${process.env.PATH_PREFIX ? process.env.PATH_PREFIX : ""}/examples/offscreen-interactivity/offscreen_interactivity_tldraw/dist/index.html`)
     if(await page.getByRole('textbox', { name: 'Host:' }).isVisible()) {
-      await passModalKey(page)
+
+      await page.getByLabel('Scheme:').selectOption(process.env.SCHEME)
+      await page.getByRole('textbox', { name: 'Host:' }).fill(process.env.HOST)
+      await page.getByRole('textbox', { name: 'Application Key:' }).fill(process.env.APPLICATION_KEY)
+      await page.getByRole('textbox', { name: 'HMAC Key:' }).fill(process.env.HMAC_KEY)
+      await page.getByRole('button', { name: 'Save' }).click()
     }
     else
     {
       await page.getByTestId('main-menu.button').click()
     }
-    await expect(page.locator('.loader')).toHaveCount(0)
+
+    await expect(page.locator('.loader')).toBeHidden({ timeout: 5_000 })
   })
 
   test("tabs should be empty", async ({ page }) => {
