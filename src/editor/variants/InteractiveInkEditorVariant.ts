9695b31fb0abcb697135295734260bc7636fa45a
Jocelyn Boureau <jocelyn.boureau@myscript.com>
2026-03-04 14:35:24 +0100

    refactor(editor): restructure editor classes and introduce EditorFactory for improved instance management

---
+++ src/editor/variants/InteractiveInkEditorVariant.ts
@@ -0,0 +1,6 @@
+/**
+ * @group Editor
+ * @hidden
+ */
+export { InteractiveInkEditor, type TInteractiveInkEditorOptions } from "../InteractiveInkEditor"
+export { InteractiveInkEditorConfiguration, type TInteractiveInkEditorConfiguration, DefaultInteractiveInkEditorConfiguration } from "../InteractiveInkEditorConfiguration"
